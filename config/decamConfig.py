a28823f5ad31be63d43aa80bb788c8174b898ca6
taranu <dtaranu@astro.princeton.edu>
2019-11-07 15:25:39 -0600

    Run meas_modelfit CModel by default in all configs

--- config/decamConfig.py
+++ config/decamConfig.py
@@ -1,3 +1,8 @@
 # use "instcal" exposures from the community pipeline until DecamIsrTask is up to snuff
 from lsst.obs.decam.decamNullIsr import DecamNullIsrTask
 config.processCcd.isr.retarget(DecamNullIsrTask)
+# Run meas_modelfit to compute CModel fluxes
+import lsst.meas.modelfit
+config.processCcd.calibrate.measurement.plugins.names |= [
+    "modelfit_DoubleShapeletPsfApprox", "modelfit_CModel"]
+config.processCcd.calibrate.measurement.slots.modelFlux = 'modelfit_CModel'
